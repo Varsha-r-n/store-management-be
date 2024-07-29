2bda2f54603b77dc7b0368f635b4fdee9bb332b9
vikram rao <vikramsscet@gmail.com>
2024-07-29 10:17:53 +0530

    create rating implementation

--- routes/rating.js
+++ routes/rating.js
@@ -3,13 +3,27 @@ var router = express.Router();
 const RatingModel = require("../modals/Rating");
 const {authenticateToken, checkAdmin} = require("../autheticateToken");
 
-/* GET ratings listing. */
+//checks if user is admin or not
+async function checkOwnAccess(req, res, next) {
+  const ratingId = req.params.id;
+  let user = await UserModel.findById(req.user.id);
+  if (user) {
+    let rating = await RatingModel.findById(ratingId);
+    if(rating.userId === req.user.id){
+      next();
+    }else{
+      res.status(401).json({ message: "not authorised" });
+    }
+  }
+}
+
+/* GET average ratings for all stores. */
 router.get("/", [authenticateToken, checkAdmin], async function (req, res) {
   let ratings = await RatingModel.find({});
   res.send(ratings);
 });
-router.get("/:id", authenticateToken, async function (req, res) {
-  let rating = await RatingModel.findById(req.params.id);
+router.get("/:store_id", authenticateToken, async function (req, res) {
+  let rating = await RatingModel.findById(req.params.store_id);
   res.send({
     email: rating.userId,
     address: rating.storeId,
@@ -21,6 +35,8 @@ router.get("/:id", authenticateToken, async function (req, res) {
 });
 router.post("/", authenticateToken, async function (req, res) {
   try {
+    req.body.userId = req.user.id;
+    console.log(req.body)
     const SaveRating = new RatingModel(req.body);
     const rating = await SaveRating.save();
     res.json({ rating });
@@ -31,13 +47,13 @@ router.post("/", authenticateToken, async function (req, res) {
 });
 
 // Delete by id
-router.delete("/:id", authenticateToken, async function (req, res) {
+router.delete("/:id", [authenticateToken, checkOwnAccess], async function (req, res) {
   const rating = await RatingModel.findByIdAndDelete(req.params.id);
   res.send(rating);
 });
 
 // Update by id
-router.put("/:id", authenticateToken, async function (req, res) {
+router.put("/:id", [authenticateToken, checkOwnAccess], async function (req, res) {
   const rating = await RatingModel.findByIdAndUpdate(req.params.id, req.body);
   res.send(rating);
 });
--- routes/store.js
+++ routes/store.js
@@ -1,38 +1,41 @@
 var express = require("express");
 var router = express.Router();
 const StoreModel = require("../modals/Store");
-//const authenticateToken = require("../autheticateToken");
+const UserModel =  require("../modals/User");
+const RoleModel = require("../modals/Role");
 const {
-    authenticateToken,
-    checkAdmin,
-    checkStoreOwnerRole,
-    checkAdminOrStoreOwnerRole
-  } = require("../autheticateToken");
+  authenticateToken,
+  checkAdminOrStoreOwnerRole
+} = require("../autheticateToken");
 
 //checks if user is admin or not
-async function checkDeleteAccess(req, res, next) {
-  let findUser = await UserModel.findById(req.user.id);
-  if (findUser) {
-    let findRole = await RoleModel.findById(findUser.userRole);
+async function checkStoreCRUDAccess(req, res, next) {
+  let user = await UserModel.findById(req.user.id);
+  if (user) {
+    let findRole = await RoleModel.findById(user.userRole);
     if (findRole && findRole.role_name === "admin") {
       next();
     } else {
+      if (findRole.role_name === "store_owner") {
         const findStore = await StoreModel.findById(req.params.id);
-        if(findStore){
-
-        }else{
-            res.status(401).json({ message: "not authorised" });
+        if (findStore.createdBy === req.user.id) {
+          next();
+        } else {
+          res.status(401).json({ message: "not authorised" });
         }
+      } else {
+        res.status(401).json({ message: "not authorised" });
+      }
     }
   }
 }
 
 /* GET stores listing. */
-router.get("/", authenticateToken, async function (req, res) {
+router.get("/", [authenticateToken], async function (req, res) {
   let stores = await StoreModel.find({});
   res.send(stores);
 });
-router.get("/:id", authenticateToken, async function (req, res) {
+router.get("/:id", [authenticateToken], async function (req, res) {
   let store = await StoreModel.findById(req.params.id);
   res.send({
     name: store.name,
@@ -43,26 +46,34 @@ router.get("/:id", authenticateToken, async function (req, res) {
     deletedDate: rating.deletedDate,
   });
 });
-router.post("/", [authenticateToken, checkAdminOrStoreOwnerRole], async function (req, res) {
-  try {
-    req.body['createdBy'] = req.user.id;
-    const Savestore = new StoreModel(req.body);
-    const store = await Savestore.save();
-    res.json({ store });
-  } catch (error) {
-    console.log(error);
-    throw error;
+router.post(
+  "/",
+  [authenticateToken, checkAdminOrStoreOwnerRole],
+  async function (req, res) {
+    try {
+      req.body["createdBy"] = req.user.id;
+      const Savestore = new StoreModel(req.body);
+      const store = await Savestore.save();
+      res.json({ store });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
-});
+);
 
 // Delete by id
-router.delete("/:id", [authenticateToken, checkDeleteAccess], async function (req, res) {
-  const store = await StoreModel.findByIdAndDelete(req.params.id);
-  res.send(store);
-});
+router.delete(
+  "/:id",
+  [authenticateToken, checkStoreCRUDAccess],
+  async function (req, res) {
+    const store = await StoreModel.findByIdAndDelete(req.params.id);
+    res.send(store);
+  }
+);
 
 // Update by id
-router.put("/:id", authenticateToken, async function (req, res) {
+router.put("/:id", [authenticateToken, checkStoreCRUDAccess], async function (req, res) {
   const store = await StoreModel.findByIdAndUpdate(req.params.id, req.body);
   res.send(store);
 });
